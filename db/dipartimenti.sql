f16535f71c98643410760f57cb005c51759afeaf
Guberlo <salasero24@gmail.com>
2020-11-29 18:03:52 +0100

    add: db table queries for testing

---
+++ db/dipartimenti.sql
@@ -0,0 +1,8 @@
+CREATE TABLE dipartimenti (
+    id int NOT NULL AUTO_INCREMENT,
+    unict_id int NOT NULL,
+    anno_accademico varchar(255),
+    nome varchar(255),
+    PRIMARY KEY (id),
+    UNIQUE (unict_id)
+)
