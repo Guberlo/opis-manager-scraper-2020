097530624ff6ed525c5969be584b4289032eece0
Guberlo <salasero24@gmail.com>
2021-03-19 19:46:06 +0100

    fix: closes issue #1

--- scraper.js
+++ scraper.js
@@ -1,18 +1,10 @@
-const { getHtmlFromUrl, sleep } = require('./app/utils');
+const { getHtmlFromUrl, tableSelectorCds, tableSelectorIns, tableSelectorDip, url, year } = require('./app/utils');
 const { extractDipStats, insertDip } = require('./app/scraper-dipartimento');
 const { extractCdsStats, insertCds } = require('./app/scraper-cds');
 const { extractInsStats, insertInsegnamento } = require('./app/scraper-insegnamento');
 const { extractFromGraphs, extractFromTable, extractSchedeStats, insertScheda } = require('./app/scraper-schede');
-const { getPrimaryID, getPrimaryIdIns, getPrimaryIdInsTest } = require('./app/db-try');
+const { closeConnection } = require('./app/db');
 const _ = require('lodash');
-
-
-const tableSelectorDip = '.col-lg-6 > table > tbody > tr:not(:first-child):not(:last-child)';
-const tableSelectorCds = '.col-lg-6 > table > tbody > tr:not(:first-child)';
-const tableSelectorIns = '.col-lg-6 > table > tbody > tr:not(:first-child)';
-
-const url = 'https://pqa.unict.it/opis/';
-const year = '2020/2021';
 
 const depRequest = getHtmlFromUrl(url);
 
@@ -38,102 +30,99 @@ async function depsAsync() {
     }
   }
 
-  async function cdsAsync(depID, dbID) {
-    const cdsUrl = `${url}cds_dip.php?id=${depID}&aa=2019`;
-  
-    const $ = await getHtmlFromUrl(cdsUrl);
-  
-    try {
-      // Process cds data
-      return Promise.all($(tableSelectorCds).map(async (i, el) => {
-        // Scrape data from cds
-        const obj = await extractCdsStats($(el), $);
-        // console.log(obj);
-  
-        // Insert data into DB
-        try {
+  async function cdsAsync(depsArray) {
+    return Promise.all(depsArray.map(async dep => {
+      const cdsUrl = `${url}cds_dip.php?id=${dep.depID}&aa=2019`;
     
-          // Push dbid to scrape cds
-          return {
-            dbID:  await insertCds(obj.cdsID, year, obj.cdsName, obj.cdsClass, dbID),
-            cdsID: obj.cdsID,
-            cdsClass: obj.cdsClass,
-          };
-        } catch(e) {
-          console.error(e);
-        }
-      }).toArray());
-    } catch (error) {
-      console.error(error);
-      return -1;
+      const $ = await getHtmlFromUrl(cdsUrl);
+    
+      try {
+        // Process cds data
+        return Promise.all($(tableSelectorCds).map(async (i, el) => {
+          // Scrape data from cds
+          const obj = await extractCdsStats($(el), $);
+          // console.log(obj);
+    
+          // Insert data into DB
+          try {
+      
+            // Push dbid to scrape cds
+            return {
+              dbID:  await insertCds(obj.cdsID, year, obj.cdsName, obj.cdsClass, dep.dbID),
+              cdsID: obj.cdsID,
+              cdsClass: obj.cdsClass,
+            };
+          } catch(e) {
+            console.error(e);
+          }
+        }).toArray());
+      } catch (error) {
+        console.error(error);
+        return -1;
+      }
+    }));
+  }
+
+  async function insAsync(cdsArray) {
+    return Promise.all(cdsArray.map(async cdsMap => {
+      return Promise.all(cdsMap.map(async cds => {
+        const insUrl = `${url}insegn_cds.php?aa=2019&cds=${cds.cdsID}&classe=${cds.cdsClass}`;
+    
+      const $ = await getHtmlFromUrl(insUrl);
+    
+      try {
+        // Process departments data
+        return Promise.all($(tableSelectorIns).map(async (i, el) => {
+          // Scrape data from departments
+          const obj = await extractInsStats($(el), $);
+        
+          try {
+            // Push dbid to scrape cds
+            return {
+              dbID: await insertInsegnamento(obj, cds.dbID),
+              linkOpis: obj.linkOpis,
+            };
+          } catch(e) {
+            console.error(e);
+          }
+        }).toArray());
+      } catch (error) {
+        console.error(error);
+        return -1;
+      }
+      }))
+    }));
+      
+  }
+
+  async function schedeAsync(insArray) {
+    for (const ins of insArray) {
+      if(_.isUndefined(ins.linkOpis) || ins.linkOpis === 'Scheda non autorizzata alla pubblicazione')
+        console.log(ins);
+
+      else {
+        const $ = await getHtmlFromUrl(url + ins.linkOpis);
+        const schedeStats = await extractSchedeStats($);
+        const grafici = extractFromGraphs($);
+        const questions = await extractFromTable($);
+        await insertScheda(schedeStats, grafici, questions, ins.dbID);
+ 
+      }
     }
   }
 
-  async function insAsync(cdsID, cdsClass, dbID) {
-    // Try to fix ECONNRESET
-    let randsleep = (Math.random() * (3000 - 1500) ) + 1500;
-    console.log(`sleeping for: ${randsleep}`);
-    await sleep(randsleep);
+  (async () => {
+    let depsArray = await depsAsync();
+    
+    let cdsArray = await cdsAsync(depsArray);
 
-    const insUrl = `${url}insegn_cds.php?aa=2019&cds=${cdsID}&classe=${cdsClass}`;
-  
-    const $ = await getHtmlFromUrl(insUrl);
-  
-    try {
-      // Process departments data
-      return Promise.all($(tableSelectorIns).map(async (i, el) => {
-        // Scrape data from departments
-        const obj = await extractInsStats($(el), $);
-       
-        try {
-          // Push dbid to scrape cds
-          return {
-            dbID: await insertInsegnamento(obj, dbID),
-            linkOpis: obj.linkOpis,
-          };
-        } catch(e) {
-          console.error(e);
-        }
-      }).toArray());
-    } catch (error) {
-      console.error(error);
-      return -1;
+    let insPromises = await insAsync(cdsArray);
+
+    insPromises = [].concat.apply([], insPromises);
+
+    for (const insArray of insPromises) {
+      schedeAsync(insArray);
     }
-  }
 
-  async function schedeAsync(dbID, link) {
-    // Try to fix ECONNRESET
-    let randsleep = (Math.random() * (30000 - 9000) ) + 9000;
-    console.log(`sleeping for: ${randsleep}`);
-    await sleep(randsleep);
-
-    const $ = await getHtmlFromUrl(link);
-  
-    const schedeStats = await extractSchedeStats($);
-  
-    const grafici = extractFromGraphs($);
-  
-    const questions = await extractFromTable($);
-  
-    await insertScheda(schedeStats, grafici, questions, dbID);
-  
-    return [schedeStats, grafici, questions];
-  
-  }
-
-  depsAsync().then(depsArray => {
-    depsArray.forEach(dep => {
-      cdsAsync(dep.depID, dep.dbID).then(cdsArray => {
-        cdsArray.forEach(async cds => {
-          insAsync(cds.cdsID, cds.cdsClass, cds.dbID).then(insArray => {
-            insArray.forEach(async ins => {
-              if(ins.linkOpis !== 'Scheda non autorizzata alla pubblicazione') {
-                const link = url + (ins.linkOpis);
-                schedeAsync(ins.dbID, link);
-              }
-            });
-          });
-        })
-      });
-    })
-  })
+    // closeConnection(); THIS NEENDS TO HAPPEN ONLY AFTER THE END OF FOR ... OF
+  })();
